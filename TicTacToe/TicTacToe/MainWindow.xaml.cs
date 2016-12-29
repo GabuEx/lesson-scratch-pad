af2b3f347f99ce4688eeb03ded462d9c645adfb8
Leafestia <lilylooseleaf@gmail.com>
2016-12-29 16:09:07 +0900

    Doots

--- TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -41,6 +41,54 @@ namespace TicTacToe
                     senderButton.Content = "X";
                 }
             }
+            if (TopLeftButton.Content != "" &&
+                TopLeftButton.Content == TopMiddleButton.Content &&
+                TopMiddleButton.Content == TopRightButton.Content)
+            {
+                //Stuff
+            }
+            else if (MiddleLeftButton.Content != "" &&
+                MiddleLeftButton.Content == MiddleMiddleButton.Content &&
+                MiddleMiddleButton.Content == MiddleRightButton.Content)
+            { 
+                //Stuff
+            }
+            else if (BottomLeftButton.Content != "" &&
+                BottomLeftButton.Content == BottomMiddleButton.Content &&
+                BottomMiddleButton.Content == BottomRightButton.Content)
+            {
+                //Stuff
+            }
+            else if (TopLeftButton.Content != "" &&
+                TopLeftButton.Content == MiddleLeftButton.Content &&
+                MiddleLeftButton.Content == BottomLeftButton.Content)
+            {
+                //Stuff
+            }
+            else if (TopMiddleButton.Content != "" &&
+                TopMiddleButton.Content == MiddleMiddleButton.Content &&
+                MiddleMiddleButton.Content == BottomMiddleButton.Content)
+            {
+                //Stuff
+            }
+            else if (TopRightButton.Content != "" &&
+                TopRightButton.Content == MiddleRightButton.Content &&
+                MiddleRightButton.Content == BottomRightButton.Content)
+            {
+                //Stuff
+            }
+            else if (TopLeftButton.Content != "" &&
+                TopLeftButton.Content == MiddleMiddleButton.Content &&
+                MiddleMiddleButton.Content == BottomRightButton.Content)
+            {
+                //Stuff
+            }
+            else if (TopRightButton.Content != "" &&
+               TopRightButton.Content == MiddleMiddleButton.Content &&
+               MiddleMiddleButton.Content == BottomLeftButton.Content)
+            {
+                //Stuff
+            }
         }
     }
 }
