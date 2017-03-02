755a323fb5580a5ac896d1a60f8c38a3f0206aa8
GabuEx <luke.longley@gmail.com>
2017-03-01 22:32:25 -0800

    Added the ability to draw the map as designed

--- SuperCoolRpg/SuperCoolRpg/SuperCoolRpg/RpgGame.cs
+++ SuperCoolRpg/SuperCoolRpg/SuperCoolRpg/RpgGame.cs
@@ -8,6 +8,9 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using System.IO;
+using System.Xml;
 
 namespace SuperCoolRpg
 {
@@ -20,6 +23,14 @@ namespace SuperCoolRpg
         SpriteBatch spriteBatch;
         Texture2D lightWarriorsTexture;
         Vector2 lightWarriorsPosition;
+
+        Texture2D mapTexture;
+        int mapTextureColumnCount;
+        int mapTileWidth;
+        int mapTileHeight;
+        int mapWidth;
+        int mapHeight;
+        int[,] mapIndexes;
 
         public RpgGame()
         {
@@ -51,6 +62,41 @@ namespace SuperCoolRpg
 
             // TODO: use this.Content to load your game content here
             lightWarriorsTexture = this.Content.Load<Texture2D>("SpriteSheets/LightWarriors");
+            mapTexture = this.Content.Load<Texture2D>("SpriteSheets/Corneria");
+
+            string mapPath = @"..\..\..\..\SuperCoolRpgContent\MapFiles\";
+            
+            XmlTextReader xmlReader = new XmlTextReader(mapPath + "Corneria.tmx");
+
+            xmlReader.ReadToFollowing("tileset");
+
+            mapTileWidth = int.Parse(xmlReader.GetAttribute("tilewidth"));
+            mapTileHeight = int.Parse(xmlReader.GetAttribute("tileheight"));
+            mapTextureColumnCount = int.Parse(xmlReader.GetAttribute("columns"));
+
+            xmlReader.ReadToFollowing("layer");
+
+            mapWidth = int.Parse(xmlReader.GetAttribute("width"));
+            mapHeight = int.Parse(xmlReader.GetAttribute("height"));
+
+            mapIndexes = new int[mapWidth, mapHeight];
+
+            xmlReader.ReadToFollowing("data");
+
+            string data = xmlReader.ReadString();
+            string[] dataLines = data.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int y = 0; y < Math.Min(dataLines.Length, mapHeight); y++)
+            {
+                string[] dataElements = dataLines[y].Split(',');
+
+                for (int x = 0; x < Math.Min(dataElements.Length, mapWidth); x++)
+                {
+                    mapIndexes[x, y] = int.Parse(dataElements[x]);
+                }
+            }
+
+            xmlReader.Close();
         }
 
         /// <summary>
@@ -107,8 +153,22 @@ namespace SuperCoolRpg
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
+
+            for (int x = 0; x < mapWidth; x++)
+            {
+                for (int y = 0; y < mapHeight; y++)
+                {
+                    int tileIndex = mapIndexes[x, y] - 1;
+                    int tileXPosition = tileIndex % mapTextureColumnCount;
+                    int tileYPosition = tileIndex / mapTextureColumnCount;
+
+                    spriteBatch.Draw(mapTexture, new Vector2(x * mapTileWidth, y * mapTileHeight), new Rectangle(tileXPosition * mapTileWidth, tileYPosition * mapTileHeight, mapTileWidth, mapTileHeight), Color.White);
+                }
+            }
+
             spriteBatch.Draw(lightWarriorsTexture, lightWarriorsPosition, new Rectangle(78, 313, 16, 16), Color.White);
             spriteBatch.Draw(lightWarriorsTexture, new Vector2(50, 50), new Rectangle(1, 40, 16, 16), Color.White);
+
             spriteBatch.End();
 
             base.Draw(gameTime);
